7b64c97c1b84d0ab2623415542fb15b17fc1868a
gulnurkaztai <kaztai.gulnur@gmail.com>
2023-02-27 18:39:14 -0500

    pages components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import SingIn from './pages/SingIn';
 
 function App() {
   return (
-    <div className="App">
-<h1>Ma App</h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore/>}/>
+          <Route path='/offers' element={<Offers/>}/>
+          <Route path='profile' element={<Profile/>}/>
+          <Route path='sign-in' element={<SingIn/>}/>
+          <Route path='sign-up' element={<SignUp/>}/>
+          <Route path='forgot-password' element={<ForgotPassword/>}/>
+        </Routes>
+        <Navbar/>
+      </Router>
+    </>
   );
 }
 
